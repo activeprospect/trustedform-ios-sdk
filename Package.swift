0841fcd1f8cc6cdc8795f59680fb381319745e0f
John Johnson <jrjohnson92@gmail.com>
2024-01-03 09:39:34 -0600

    Swift package 0.3.1 (#12)
    * swift package
    
    * folder structure
    
    * fixup
    
    * wip
    
    * wip
    
    * update framework to support 5.7
    
    * deploy version
    
    * fixup
    
    * fixup
    
    * signed
    
    * revert

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TrustedFormPackage",
+    platforms: [.iOS(.v12)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TrustedFormPackage",
+            targets: [
+                "TrustedFormPackage",
+                "TrustedFormSwift"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.8.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TrustedFormPackage",
+            dependencies: [
+                .product(name: "Alamofire", package: "Alamofire"),
+                .target(name: "TrustedFormSwift", condition: .when(platforms: [.iOS]))
+            ],
+            path: "TrustedFormPackage/Sources"
+        ),
+        .binaryTarget(
+            name: "TrustedFormSwift",
+            path: "TrustedFormSwift.xcframework"
+        )
+    ]
+)
